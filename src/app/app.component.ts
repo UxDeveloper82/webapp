3d42a5b429e160d54f99b94378529c662a12ee7f
Martin Fernandez <Electronicsmf@aol.com>
2024-02-11 00:20:19 -0500

    pagination in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webapp';
+  list_items: string[] = [
+    "James", "Robert", "John", "Michael", "David", "William", "Richard", "Joseph", "Thomas",
+    "Christopher", "Charles", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven",
+    "Andrew", "Paul", "Joshua", "Kenneth", "Kevin"
+  ];
+
+
+  current_page: number = 1;
+  rows: number = 5;
+
+  DisplayList(page: number) {
+      const start = this.rows * (page -1);
+      const end = start + this.rows;
+      return this.list_items.slice(start, end);
+  }
+
+  SetupPagination() {
+    const page_count = Math.ceil(this.list_items.length / this.rows);
+    return Array.from({ length: page_count}, (_,i) => i + 1);
+  }
+
+  onPageChange(page: number) {
+    this.current_page = page;
+  }
+
+
 }
